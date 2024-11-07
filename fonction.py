38d672fe5855f9588497b77d54dbd09895762a28
Loupijellal <gaspard.crem@gmail.com>
2024-11-07 08:59:15 +0100

    Add files via upload

---
+++ fonction.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+# Création de la fenêtre principale
+fenetre = tk.Tk()
+fenetre.title("Canevas Tkinter")
+fenetre.geometry("500x400")
+
+# Création d'un canevas
+canevas = tk.Canvas(fenetre, width=400, height=300, bg="white")
+canevas.pack(pady=20)
+
+
+
+
+
+# Lancer la boucle principale de la fenêtre
+fenetre.mainloop()
