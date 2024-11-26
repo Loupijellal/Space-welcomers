4019499d365f8f8419e29789fe48ad86001263a4
annab444 <annabelle.rmbd@gmail.com>
2024-11-26 10:33:39 +0100

    test

--- fonction.py
+++ fonction.py
@@ -1,27 +1,27 @@
 import tkinter as tk
 
-
 # Fonction pour démarrer le jeu
-def demarrer_jeu(canevas,id_bouton_demarrer,id_bouton_quitter,id_bouton_options, image_alien_tk, largeur_ecran, image_vaisseau_tk):
+def demarrer_jeu(canevas,id_bouton_demarrer,id_bouton_quitter,id_bouton_options, image_alien_tk):
     canevas.delete(id_bouton_demarrer)
     canevas.delete(id_bouton_quitter)
     canevas.delete(id_bouton_options)
     #mise_a_jour_score()
-    global alien , vaisseau
+    global alien
     alien = canevas.create_image(200, 300, image=image_alien_tk, anchor="center")
-    vaisseau = canevas.create_image(largeur_ecran/2, 600, image=image_vaisseau_tk, anchor="center")
     canevas.tag_raise(alien)
       # Démarrer le mouvement de l'Alien
 
+    
+
 # Déplacement du vaisseau
-def deplacer_vaisseau(event,canevas, dx):
+def deplacer_vaisseau(event,canevas, vaisseau, dx):
     if event.keysym == "Left":
         canevas.move(vaisseau, -dx, 0)
     elif event.keysym == "Right":
         canevas.move(vaisseau, dx, 0)
 
 # Tirer un projectile
-def tirer_projectile(event, canevas, projectiles):
+def tirer_projectile(event, canevas, vaisseau, projectiles):
     x, y = canevas.coords(vaisseau)
     projectile = canevas.create_oval(x - 5, y - 20, x + 5, y - 10, fill="red")
     projectiles.append(projectile)
